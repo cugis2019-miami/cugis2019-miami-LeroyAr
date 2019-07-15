ce8b89f6b3062acb13dc719af6a54426f1612aa0
LeroyAr <52932096+LeroyAr@users.noreply.github.com>
2019-07-15 15:51:51 -0400

    Add files via upload

---
+++ Python Introduction.py
@@ -0,0 +1,38 @@
+#print(53)
+#53
+
+#"Hello World"
+
+#print(54)
+#Print(54)
+
+#(8/9) * 3
+
+#print("The division of 5 by 3 is" , 5/3)
+
+#Create function to sum any two numbers
+
+def sum(a,b,c):
+    sum = a + b + c
+    print(sum)
+    
+sum(2,3,4)
+
+def areaTriangle(b,h):
+    areaTriangle = (1/2) * b * h
+    print("The area of the triangle is:",areaTriangle)
+
+areaTriangle (4,6)
+
+def areaSquare(a):
+    areaSquare = a ** 2
+    print (areaSquare)
+    
+areaSquare(4)
+
+import math
+def areaOctagon(a):
+    areaOctagon = 2*(1+math.sqrt(2))*(a**2)
+    print (areaOctagon)
+
+areaOctagon(4)
